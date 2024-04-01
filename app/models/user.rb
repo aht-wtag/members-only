019449d405c54807490443a62396e1fdf30c0137
aht-wtag <tahsin.ahmed@welldev.io>
2024-04-01 20:34:06 +0600

    devise set up and working for authenticated users to create new posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_secure_password
     has_many :posts
-    
     validates :username, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
 end
