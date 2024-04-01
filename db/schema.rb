66b37a0a5a38e5f8e2b632f4e855e6ec089d167d
aht-wtag <tahsin.ahmed@welldev.io>
2024-04-01 17:10:14 +0600

    Post model created and associates made between posts and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_110323) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_110603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "tilte"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
