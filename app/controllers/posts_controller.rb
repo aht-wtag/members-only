6c8939fc20c320e275cbded6b7aef6ad6484521c
aht-wtag <tahsin.ahmed@welldev.io>
2024-04-01 20:10:00 +0600

    posts_controller changed for authenticated users, strong params included, create method included

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!, except: :index
     def index
         @post = Post.all
     end
 
     def new
         @post = Post.new
+    end
+    
+    def create
+        @post = current_user.posts.build(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end 
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :body)
     end
 end
